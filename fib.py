58c0bd0c51f7948d0ab0ac8ad6e6011089dd158b
melanie dotson <jaqueline@csm-wl-dhcp-199-28.mines.edu>
2019-11-21 18:43:10 -0700

    final version

---
+++ fib.py
@@ -0,0 +1,16 @@
+# fibonacci.py
+
+def fib():
+    fibs = [1, 2]
+
+    for i in range(1,99):
+        fibs.append(fibs[i]+fibs[i-1])
+
+    print(len(fibs))
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
